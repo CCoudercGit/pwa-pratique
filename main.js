a8f224330eba1a728cca2c9170dafea789ead9f7
CCouderc <clementaja@free.fr>
2020-09-24 11:18:52 +0200

    Commit de plein de fichiers???

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log('hello depuis main');
+const technosDiv = document.querySelector('#technos');
+
+function loadTechnologies(technos) {
+    fetch('http://localhost:3001/technos')
+        .then(response => {
+            response.json()
+                .then(technos => {
+                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
+                            .join('');
+            
+                    technosDiv.innerHTML = allTechnos; 
+                });
+        })
+        .catch(console.error);
+}
+
+loadTechnologies(technos);
