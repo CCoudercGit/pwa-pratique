e0be7ad2dd423fdb8c9e10259d19a42dab744066
CCouderc <clementaja@free.fr>
2020-09-24 13:45:44 +0200

    Encore un screen ENORME. Ah oui et le CSS aussi. C'est important

--- main.js
+++ main.js
@@ -1,18 +1,48 @@
-console.log('hello depuis main');
+	
 const technosDiv = document.querySelector('#technos');
-
+	
+ 
+	
 function loadTechnologies(technos) {
+	
     fetch('http://localhost:3001/technos')
+	
         .then(response => {
+	
             response.json()
+	
                 .then(technos => {
-                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
+	
+                    const allTechnos = technos.map(t => `
+	
+                    <div class="card">
+	
+                        <div class="card-body">
+	
+                        <h5 class="card-title">${t.name}</h5>
+	
+                        <p class="card-text">${t.description}</p>
+	
+                        <a href="${t.url}" class="card-link">site de ${t.name}</a>
+	
+                        </div>
+	
+                    </div>`)
+	
                             .join('');
+	
             
+	
                     technosDiv.innerHTML = allTechnos; 
+	
                 });
+	
         })
+	
         .catch(console.error);
+	
 }
-
+	
+ 
+	
 loadTechnologies(technos);
