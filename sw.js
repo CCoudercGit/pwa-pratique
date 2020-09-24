7b6717d9eab65978edf15cdbf125a652dca2589e
CCouderc <clementaja@free.fr>
2020-09-24 16:21:42 +0200

    5. Gestion du cache avancée - résultat

--- sw.js
+++ sw.js
@@ -1,28 +1,47 @@
-const cacheName = 'veille-techno' + '1.1';
-
-self.addEventListener('install', (evt) => {
-    console.log(`sw installé à ${new Date().toLocaleTimeString()}`);
-
-    const cachePromise = caches.open(cacheName).then(cache => {
-        return cache.addAll([
-            'index.html',
-            'main.js',
-            'style.css',
-            'vendors/bootstrap4.min.css',
-            'add_techno.html',
-            'add_techno.js',
-            'contact.html',
-            'contact.js',
-        ])
-        .then(console.log('cache initialisé'))
-        .catch(console.err);
-    });
-
-    evt.waitUntil(cachePromise);
-
+//const cacheName = 'veille-techno' + '1.1';
+	
+const cacheName = 'veille-techno' + '1.2';
+	
+self.addEventListener('fetch', (evt) => {
+	
+ 
+	
+    // 5.3 Stratégie de network first with cache fallback
+	
+    // On doit envoyer une réponse
+	
+    evt.respondWith(
+	
+        // on doit d'abord faire une requête sur le réseau de ce qui a été intercepté
+	
+        fetch(evt.request).then(res => {
+	
+            console.log("url récupérée depuis le réseau", evt.request.url);
+	
+            // mettre dans le cache le résultat de cette réponse : en clef la requête et en valeur la réponse
+	
+            caches.open(cacheName).then(cache => cache.put(evt.request, res));
+	
+            // quand on a la réponse on la retourne (clone car on ne peut la lire qu'une fois)
+	
+            return res.clone();
+	
+        })
+	
+        // Si on a une erreur et que l'on arrive pas à récupérer depuis le réseau, on va chercher dans le cache
+	
+        .catch(err => {
+	
+            console.log("url récupérée depuis le cache", evt.request.url);
+	
+            return caches.match(evt.request);
+	
+        })
+	
+    );
 });
 
-self.addEventListener('fetch', (evt) => {
+/* self.addEventListener('fetch', (evt) => {
     // 3.4
     if(!navigator.onLine) {
         const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
@@ -75,4 +94,21 @@ self.addEventListener('fetch', (evt) => {
             )
         );
     });
+});
+
+*/
+
+self.addEventListener('activate', (evt) => {
+    console.log(`sw activé à ${new Date().toLocaleTimeString()}`); 
+  
+    // 5.4 Supprimer les anciennes instances de cache
+    let cacheCleanPromise = caches.keys().then(keys => {
+        keys.forEach(key => {            
+            if(key !== cacheName){
+                caches.delete(key);
+            }
+        });
+    });
+
+    evt.waitUntil(cacheCleanPromise);
 });
