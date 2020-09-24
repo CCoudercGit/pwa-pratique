ff3e63648ffd3c42c3834bdd9f12747d0a52bbc6
CCouderc <clementaja@free.fr>
2020-09-24 16:08:25 +0200

    J'adore les screen. 5-2.

--- sw.js
+++ sw.js
@@ -32,14 +32,47 @@ self.addEventListener('fetch', (evt) => {
     console.log('sw intercepte la requête suivante via fetch', evt);
     console.log('url interceptée', evt.request.url);
 
-     // 4.7 : Récupérer les réponses depuis le cache
-     evt.respondWith(
-        caches.match(evt.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
+    self.addEventListener('fetch', (evt) => {
+        if(!navigator.onLine) {
+            const headers = { headers: { 'Content-Type': 'text/html;charset=utf-8'} };
+            evt.respondWith(new Response('<h1>Pas de connexion internet</h1><div>Application en mode dégradé. Veuillez vous connecter</div>', headers));
+        }
+    
+        // console.log('sw intercepte la requête suivante via fetch', evt);
+        console.log('url interceptée', evt.request.url);
+    
+    
+        // 5.1 Stratégie : cache only with network callback
+        evt.respondWith(
+        
+            caches.match(evt.request)
+                .then(cachedResponse => {   
+                    if (cachedResponse) {
+                           // identification de la requête trouvée dans le cache
+                        console.log("url depuis le cache", evt.request.url);
+                        return cachedResponse;
+                    }
+    
+                    // 5.1 Stratégie de cache
+                    return fetch(evt.request).then(
+                        // On récupère la requête
+                        function(newResponse) {
+                            // identification de la requête ajoutée au cache
+                            console.log("url depuis le réseau et mise en cache", evt.request.url);
+                            
+                            // Accès au cache
+                            caches.open(cacheName).then(
+                                function(cache){
+                                    // ajout du résultat de la requête au cache
+                                    cache.put(evt.request, newResponse);
+                                }
+                            );
+                            // Utilisation de clone car on ne peut utiliser qu'une fois la réponse
+                            return newResponse.clone();
+                        }
+                    )
                 }
-                return fetch(evt.request);
-            })
-    );
+            )
+        );
+    });
 });
